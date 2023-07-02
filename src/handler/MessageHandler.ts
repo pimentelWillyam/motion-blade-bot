5f28685c9b786e687a6734b764f154e85eb2a1ef
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-02 07:49:32 -0300

    refactor: adding dice features

--- src/handler/MessageHandler.ts
+++ src/handler/MessageHandler.ts
@@ -6,27 +6,30 @@ import { type Message } from "discord.js"
 
 class MessageHandler implements IMessageHandler {
 
-  constructor(private readonly commandHandler: ICommandManager) {}
+  constructor(private readonly commandManager: ICommandManager) {}
 
   isACommand (message: string): boolean {
     if (message[0] == '!') return true
     else return false
   }
 
-  handle (message: Message) {
+  handle (message: Message):void {
     if (message.author.username == 'RPG Master') return 
-    if (!this.isACommand(message.content)) return
-    if (message.content == '!d20') {
-      this.commandHandler.roll(20)
+    else if (!this.isACommand(message.content)) return
+    else if (message.content == '!d20') {
+      message.reply(this.commandManager.rollDice(20).toString())
     }
-    if (message.content == '!d10') {
-      this.commandHandler.roll(10)
+    else if (message.content == '!d10') {
+      message.reply(this.commandManager.rollDice(10).toString())
+
     }
-    if (message.content == '!d5') {
-      this.commandHandler.roll(5)
+    else if (message.content == '!d5') {
+      message.reply(this.commandManager.rollDice(5).toString())
+
     }
-    if (message.content == '!d2') {
-      this.commandHandler.roll(2)
+    else if (message.content == '!d2') {
+      message.reply(this.commandManager.rollDice(2).toString())
+
     }
   }
 }
