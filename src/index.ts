37a0610c23b3367a13db273da0e30817c74f240a
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-02 07:49:13 -0300

    refactor: importing and instanciating random number generator and command handler, also injecting them at the discord client

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,38 @@ import * as dotenv from 'dotenv-safe'
 
 dotenv.config()
 
+// importing the random number generator
+import RandomNumberGenerator from './helper/randomNumberGenerator';
+
+//importing command manager
+import CommandManager from './helper/CommandManager'
 // importing message handler
-import MessageHandler from './handler/MessageHandler';
+import MessageHandler from './handler/MessageHandler'
 
 // importing discord library
 import {Client, IntentsBitField} from 'discord.js'
 
 // importing discord client
-import DiscordClient from './helper/DiscordClient';
+import DiscordClient from './helper/DiscordClient'
 
 //instanciating discord.js client
 const client = new Client({ intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMembers, IntentsBitField.Flags.GuildMessages,IntentsBitField.Flags.MessageContent]})
 
+// instanciating the random number generator
+const randomNumberGenerator = new RandomNumberGenerator()
+
+// instanciating the command manager
+const commandManager = new CommandManager(randomNumberGenerator)
 // instanciating message handler
-const messageHandler = new MessageHandler()
+const messageHandler = new MessageHandler(commandManager)
 
 //instanciating discord manipulation class
 const discordClient = new DiscordClient(client, messageHandler)
 
 discordClient.start()
 discordClient.listen()
+client.login(process.env.token)
+
 
 
 
