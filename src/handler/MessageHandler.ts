5aa3f475c8cc1e958aca699668787d0a4f9c0f23
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-01 22:48:24 -0300

    refactor: adding command handler to the message handler

--- src/handler/MessageHandler.ts
+++ src/handler/MessageHandler.ts
@@ -1,9 +1,13 @@
 import type IMessageHandler from "../interface/IMessageHandler"
+import type ICommandManager from '../interface/ICommandManager'
 
 import { type Message } from "discord.js"
 
 
 class MessageHandler implements IMessageHandler {
+
+  constructor(private readonly commandHandler: ICommandManager) {}
+
   isACommand (message: string): boolean {
     if (message[0] == '!') return true
     else return false
@@ -12,6 +16,18 @@ class MessageHandler implements IMessageHandler {
   handle (message: Message) {
     if (message.author.username == 'RPG Master') return 
     if (!this.isACommand(message.content)) return
+    if (message.content == '!d20') {
+      this.commandHandler.roll(20)
+    }
+    if (message.content == '!d10') {
+      this.commandHandler.roll(10)
+    }
+    if (message.content == '!d5') {
+      this.commandHandler.roll(5)
+    }
+    if (message.content == '!d2') {
+      this.commandHandler.roll(2)
+    }
   }
 }
 
