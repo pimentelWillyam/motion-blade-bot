5eb4e643338e02c459b06fa1731a9fcb0d746faf
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-01 21:46:24 -0300

    feat: adding correct constants, using correct constants at client instantiation and adding listener

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,19 @@ import * as dotenv from 'dotenv-safe'
 dotenv.config()
 
 // importing discord library
-import {Client} from 'discord.js'
+import {Client, IntentsBitField} from 'discord.js'
 
 // importing discord client
 import DiscordClient from './helper/DiscordClient';
 
 //instanciating discord.js client
-const client = new Client({intents: ['MessageContent', 'DirectMessages' ]});
+const client = new Client({ intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMembers, IntentsBitField.Flags.GuildMessages,IntentsBitField.Flags.MessageContent]})
 
 //instanciating discord manipulation class
 const discordClient = new DiscordClient(client)
 
 discordClient.start()
+discordClient.listen()
 
 
 
