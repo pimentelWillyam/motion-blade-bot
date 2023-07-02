8e565e65b9cd231097da18df582d196a5ca7d2f2
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-01 22:48:57 -0300

    refactor: injecting dice manager at the command manager

--- src/helper/CommandManager.ts
+++ src/helper/CommandManager.ts
@@ -1,8 +1,12 @@
 import type ICommandManager from '../interface/ICommandManager'
+import type IDiceManager from '../interface/IDiceManager'
 
 
 class CommandManager implements ICommandManager {
-
+  constructor(private readonly diceManager: IDiceManager) {}
+  rollDice(diceSides: number): number {
+    return this.diceManager.roll(diceSides)
+  }
 }
 
 export default CommandManager
