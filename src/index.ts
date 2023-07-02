5738b3ddf4a0372fb6b5236adbfa9753b0fd94d8
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-01 21:59:04 -0300

    refactor: importingmessage handler and injecting at the discord client

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import * as dotenv from 'dotenv-safe'
 
 dotenv.config()
 
+// importing message handler
+import MessageHandler from './handler/MessageHandler';
+
 // importing discord library
 import {Client, IntentsBitField} from 'discord.js'
 
@@ -12,8 +15,11 @@ import DiscordClient from './helper/DiscordClient';
 //instanciating discord.js client
 const client = new Client({ intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMembers, IntentsBitField.Flags.GuildMessages,IntentsBitField.Flags.MessageContent]})
 
+// instanciating message handler
+const messageHandler = new MessageHandler()
+
 //instanciating discord manipulation class
-const discordClient = new DiscordClient(client)
+const discordClient = new DiscordClient(client, messageHandler)
 
 discordClient.start()
 discordClient.listen()
