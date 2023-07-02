ff9232793fba0fc38153b37d8f4b8f2ee19fd0ec
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-01 21:46:41 -0300

    feat: adding message handler class

---
+++ src/handler/MessageHandler.ts
@@ -0,0 +1,19 @@
+import type IMessageHandler from "../interface/IMessageHandler"
+
+import { type Message } from "discord.js"
+
+
+class MessageHandler implements IMessageHandler {
+  isACommand (message: string): boolean {
+    if (message[0] == '!') return true
+    else return false
+  }
+
+  handle (message: Message) {
+    if (message.author.username == 'RPG Master') return 
+    if (!this.isACommand(message.content)) return
+    
+  }
+}
+
+export default MessageHandler
