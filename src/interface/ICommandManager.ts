625532a4360c38a072bf21f1a12f5bc0cfe6f12c
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-03 10:51:36 -0300

    refactor: adding help, roll and servant creation methods to the command manager interface

--- src/interface/ICommandManager.ts
+++ src/interface/ICommandManager.ts
@@ -1,5 +1,10 @@
+import type {Message} from 'discord.js'
+
 interface CommandManager {
-  rollDice(diceSides: number): number
-}
+  help(message: Message): void
+  roll(message: Message, diceSides: number): void
+  createServant(message: Message, name: string, profession: string ): void
+  
+  }
 
 export default CommandManager
