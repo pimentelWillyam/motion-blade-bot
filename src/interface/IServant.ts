536f7b1102cd304f7c14dbfc44fa8bf0f2526680
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-02 21:11:01 -0300

    feat: adding servant interface

---
+++ src/interface/IServant.ts
@@ -0,0 +1,18 @@
+
+
+interface IServant {
+  masterId: string
+  name: string
+  profession: string
+  seniority: number
+  fortitude: number
+  agility: number
+  strength: number
+  technique: number
+  isInBattle: boolean
+  battlePosition: [number, number]
+  
+
+}
+
+export default IServant
