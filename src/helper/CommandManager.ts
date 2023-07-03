c1863e391bf40bb2235a1bd733259e32cdd1a888
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-03 10:44:09 -0300

    refactor: adding roll, help and servant creation commands

--- src/helper/CommandManager.ts
+++ src/helper/CommandManager.ts
@@ -1,12 +1,34 @@
 import type ICommandManager from '../interface/ICommandManager'
 import type IRandomNumberGenerator from '../interface/IRandomNumberGenerator'
-
+import type IServantManager from '../interface/IServantManager'
+import { type Message } from 'discord.js';
 
 class CommandManager implements ICommandManager {
-  constructor(private readonly randomNumberGenerator: IRandomNumberGenerator) {}
-  rollDice(diceSides: number): number {
-    return this.randomNumberGenerator.generate(1, diceSides)
+  constructor(private readonly randomNumberGenerator: IRandomNumberGenerator, private readonly servantManager: IServantManager) {}
+
+  help(message: Message<boolean>): void {
+    const guideMessage = `
+    Olá, sou o Bot responsável por administrar as operações relacionadas ao RPG. Atualmente, a lista de funcionalidades está em expansão, as funcionalidades que o bot possui até agora são:\n
+    1- Help
+      A função help é responsável por informar sobre os comandos existentes e como aplica-los.
+      Ela é usada no formato: !help
+    
+    2- Rolar
+      A função rolar é responsável por rolar um dado de n lados
+      Ela é usada no formato: !rolar (nLados)
+    `
+
+    message.reply(guideMessage)  
   }
+
+  roll(message: Message, diceSides: number): void {
+    message.reply(this.randomNumberGenerator.generate(1, diceSides).toString())
+  }
+
+  createServant(message: Message<boolean>, name: string, profession: string): void {
+    console.log('this function is incomplete')
+  }
+
 }
 
 export default CommandManager
