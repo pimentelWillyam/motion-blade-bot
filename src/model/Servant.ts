1b7144c268919b37f2545e4fca7054382b00a897
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-03 07:49:40 -0300

    refactor: adding atributes to the interface, renaming class to profession

--- src/model/Servant.ts
+++ src/model/Servant.ts
@@ -3,21 +3,15 @@ import type IServant from "../interface/IServant";
 // força, técnica, agilidade e fortitude
 
 class Servant implements IServant {
-  class: string
   seniority: number
-  strength: number
-  technique: number
-  agility: number
-  fortitude: number
+  atributes: Atributes;
   isInBattle: boolean
   battlePosition: [number, number]
-  constructor(readonly masterId: string, readonly name: string, readonly profession: string, readonly type: string, agility: number, technique: number, strength: number, fortitude: number) {
-    this.class = ''
+
+  constructor(readonly id: string, readonly masterId: string, readonly name: string, readonly profession: Profession, readonly type: string, agility: number, technique: number, strength: number, fortitude: number) {
+    this.profession = profession
     this.seniority = 0
-    this.agility = agility
-    this.technique = technique
-    this.strength = strength
-    this.fortitude = fortitude
+    this.atributes = {agility: agility, strength: strength, technique: technique, fortitude: fortitude}
     this.isInBattle = false
     this.battlePosition = [-1, -1]
   }
