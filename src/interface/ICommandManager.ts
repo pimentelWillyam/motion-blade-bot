d5cd9fcd89ed6c17472ee6090acab8cf6ef8f6a8
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-03 11:16:18 -0300

    fix: correcting the command manager interface import

--- src/interface/ICommandManager.ts
+++ src/interface/ICommandManager.ts
@@ -1,10 +1,10 @@
 import type {Message} from 'discord.js'
 
-interface CommandManager {
+interface ICommandManager {
   help(message: Message): void
   roll(message: Message, diceSides: number): void
   createServant(message: Message, name: string, profession: string ): void
   
   }
 
-export default CommandManager
+export default ICommandManager
