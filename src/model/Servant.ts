9084a0d0399c936971b956332eaf24d1a96dbced
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-28 22:35:46 -0300

    refactor: adding attributes to respective professions

--- src/model/Servant.ts
+++ src/model/Servant.ts
@@ -4,14 +4,65 @@ import type IServant from "../interface/IServant";
 
 class Servant implements IServant {
   seniority: number
-  atributes: Atributes;
+  atributes: atributes;
   isInBattle: boolean
   battlePosition: [number, number]
 
-  constructor(readonly id: string, readonly masterId: string, readonly name: string, readonly profession: Profession, readonly type: string, agility: number, technique: number, strength: number, fortitude: number) {
+  constructor(readonly id: string, readonly masterId: string, readonly name: string, readonly profession: profession, readonly type: string) {
     this.profession = profession
     this.seniority = 0
-    this.atributes = {agility: agility, strength: strength, technique: technique, fortitude: fortitude}
+    if (profession == 'Barbarian'){
+      this.atributes = {
+        agility: 4, 
+        strength: 4, 
+        technique: 0, 
+        fortitude: 0
+      }
+    }
+    else if (profession == 'Hunter'){
+      this.atributes = {
+        agility: 3, 
+        strength: 0, 
+        technique: 5, 
+        fortitude: 0
+      }
+    }
+    else if (profession == 'Knight'){
+      this.atributes = {
+        agility: 0, 
+        strength: 0, 
+        technique: 0, 
+        fortitude: 8
+      }
+    }
+    else if (profession == 'Squire'){
+      this.atributes = {
+        agility: 0, 
+        strength: 0, 
+        technique: 4, 
+        fortitude: 4
+      }
+    }
+    else if (profession == 'Thief'){
+      this.atributes = {
+        agility: 5, 
+        strength: 0, 
+        technique: 3, 
+        fortitude: 0
+      }
+    }
+    else if (profession == 'Infantryman'){
+      this.atributes = {
+        agility: 0, 
+        strength: 3, 
+        technique: 0, 
+        fortitude: 5
+      }
+    }
+    else{
+      throw new Error(`Invalid profession`)
+    }
+    
     this.isInBattle = false
     this.battlePosition = [-1, -1]
   }
