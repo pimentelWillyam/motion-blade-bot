a3d81082b490e01978d1aececd94a4ba9277bcf6
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-29 10:54:27 -0300

    refactor: removing string parameter

--- src/interface/IServantManager.ts
+++ src/interface/IServantManager.ts
@@ -1,5 +1,8 @@
+import type Servant from "../model/Servant"
+import type Profession from "../type/Profession"
+
 interface IServantManager {
-  createServant: (masterId: string, name: string, profession: string) => boolean
+  createServant: (masterId: string, name: string, profession: Profession) => Servant
   deleteServant: (servantMasterId: string, servantId: string) => boolean
 }
 
