00142d25fb83cdc57801eee844c8b603b7a74b51
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-29 11:27:32 -0300

    refactor: adding attributes type; adding get attributes method; adding getSevantAttributes method

--- src/interface/IServantManager.ts
+++ src/interface/IServantManager.ts
@@ -1,9 +1,12 @@
 import type Servant from "../model/Servant"
+import type Attributes from "../type/Attributes"
 import type Profession from "../type/Profession"
 
 interface IServantManager {
   createServant: (masterId: string, name: string, profession: Profession) => Servant
   deleteServant: (servantMasterId: string, servantId: string) => boolean
+  getAttributes: (profession: string) => Attributes  
+  getServantAttributes: (name: string) => Attributes
 }
 
 export default IServantManager
