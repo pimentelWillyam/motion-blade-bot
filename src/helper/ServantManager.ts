14e72ad17c20083b104fc10e4a6125c77524e105
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-29 08:31:03 -0300

    refactor: adding get attributes method to servant manager

--- src/helper/ServantManager.ts
+++ src/helper/ServantManager.ts
@@ -14,6 +14,62 @@ class ServantManager implements IServantManager{
     console.log('deleteServant')
     return true
   }
+
+  getAttributes = (profession: string): attributes => {
+    let attributes: attributes
+    if (profession == 'Barbarian'){
+      attributes = {
+        agility: 4, 
+        strength: 4, 
+        technique: 0, 
+        fortitude: 0
+      }
+    }
+    else if (profession == 'Hunter'){
+      attributes = {
+        agility: 3, 
+        strength: 0, 
+        technique: 5, 
+        fortitude: 0
+      }
+    }
+    else if (profession == 'Knight'){
+      attributes = {
+        agility: 0, 
+        strength: 0, 
+        technique: 0, 
+        fortitude: 8
+      }
+    }
+    else if (profession == 'Squire'){
+      attributes = {
+        agility: 0, 
+        strength: 0, 
+        technique: 4, 
+        fortitude: 4
+      }
+    }
+    else if (profession == 'Thief'){
+      attributes = {
+        agility: 5, 
+        strength: 0, 
+        technique: 3, 
+        fortitude: 0
+      }
+    }
+    else if (profession == 'Infantryman'){
+      attributes = {
+        agility: 0, 
+        strength: 3, 
+        technique: 0, 
+        fortitude: 5
+      }
+    }
+    else{
+      throw new Error('Invalid profession')
+    }
+    return attributes
+  }
 }
 
 export default ServantManager
