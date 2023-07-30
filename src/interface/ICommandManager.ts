9b58ce282b60011163863c3777401f205aadf589
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-30 09:05:45 -0300

    refactor: adding classes method to the command manager interface

--- src/interface/ICommandManager.ts
+++ src/interface/ICommandManager.ts
@@ -2,6 +2,7 @@ import type {Message} from 'discord.js'
 
 interface ICommandManager {
   help(message: Message): void
+  classes(message: Message): void
   roll(message: Message, diceSides: number): void
   createServant(message: Message, name: string, profession: string ): void
   getServantAttributes(message: Message, name: string): void
