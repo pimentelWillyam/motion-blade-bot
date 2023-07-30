686dad75ba2442d70b84087071d4afcbf796bc83
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-29 21:33:49 -0300

    refactor: adding get servant by name method to the interface

--- src/interface/IServantManager.ts
+++ src/interface/IServantManager.ts
@@ -6,6 +6,7 @@ interface IServantManager {
   createServant: (masterId: string, name: string, profession: Profession) => Servant
   deleteServant: (servantMasterId: string, servantId: string) => boolean
   getAttributes: (profession: string) => Attributes  
+  getServantByName: (name: string) => Servant
   getServantAttributes: (name: string) => Attributes
   applyDamageToServant: (name: string, damage: number) => Attributes
 }
