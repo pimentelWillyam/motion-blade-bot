f0d6c1f1f70b070325986584f32b9d8ea9b4f19a
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-08-19 11:17:14 -0300

    refactor: adding strength roll

--- src/handler/MessageHandler.ts
+++ src/handler/MessageHandler.ts
@@ -36,6 +36,9 @@ class MessageHandler implements IMessageHandler {
     else if (treatedMessage[0] === 'tecnica'){
       this.commandManager.rollServantTechnique(message, treatedMessage[1])
     }
+    else if (treatedMessage[0] === 'força'){
+      this.commandManager.rollServantStrength(message, treatedMessage[1])
+    }
     else{
       message.reply('Comando inexistente')
     }
