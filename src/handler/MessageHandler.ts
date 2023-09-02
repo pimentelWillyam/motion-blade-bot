f1ab25dd9f7c381b51d20048061b5a8080351149
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-09-02 19:03:37 -0300

    refactor: making commands more strict

--- src/handler/MessageHandler.ts
+++ src/handler/MessageHandler.ts
@@ -12,43 +12,43 @@ class MessageHandler implements IMessageHandler {
     if (message.author.username == 'RPG Master') return 
     else if (!this.isACommand(message.content)) return   
     const treatedMessage =  this.treatMessage(message.content)
-    if ( treatedMessage[0] == 'ajuda'){
+    if ( treatedMessage[0] == 'ajuda' && treatedMessage.length == 1){
       this.commandManager.help(message)
     }
-    else if ( treatedMessage[0] == 'classes'){
+    else if ( treatedMessage[0] == 'classes' && treatedMessage.length == 1) {
       this.commandManager.classes(message)
     }
     else if (treatedMessage[0] == 'rolar' && treatedMessage.length == 2){
       this.commandManager.roll(message, parseInt(treatedMessage[1]) )
     }
-    else if (treatedMessage[0] == 'criar' && treatedMessage[1] == 'servo'){
+    else if (treatedMessage[0] == 'criar' && treatedMessage[1] == 'servo' && treatedMessage.length == 4){
       this.commandManager.createServant(message, treatedMessage[2], treatedMessage[3] as Profession)
     }
-    else if (treatedMessage[0] == 'atributos'){
+    else if (treatedMessage[0] == 'atributos' && treatedMessage.length == 2){
       this.commandManager.getServantAttributes(message, treatedMessage[1])
     }
-    else if (treatedMessage[0] == 'aplicar' && treatedMessage[1] == 'dano'){
+    else if (treatedMessage[0] == 'aplicar' && treatedMessage[1] == 'dano'  && treatedMessage.length == 4){
       this.commandManager.applyDamageToServant(message, treatedMessage[2], parseInt(treatedMessage[3]))
     }
-    else if (treatedMessage[0] === 'agilidade'){
+    else if (treatedMessage[0] === 'agilidade' && treatedMessage.length == 2){
       this.commandManager.rollServantAgility(message, treatedMessage[1])
     }
-    else if (treatedMessage[0] === 'tecnica'){
+    else if (treatedMessage[0] === 'tecnica' && treatedMessage.length == 2){
       this.commandManager.rollServantTechnique(message, treatedMessage[1])
     }
-    else if (treatedMessage[0] === 'força'){
+    else if (treatedMessage[0] === 'força' && treatedMessage.length == 2){
       this.commandManager.rollServantStrength(message, treatedMessage[1])
     }
-    else if (treatedMessage[0] === 'fortitude'){
+    else if (treatedMessage[0] === 'fortitude' && treatedMessage.length == 2){
       this.commandManager.rollServantFortitude(message, treatedMessage[1])
     }
-    else if (treatedMessage[1] === 'ataca'){
+    else if (treatedMessage[1] === 'ataca' && treatedMessage.length == 3){
       this.commandManager.rollServantAttack(message, treatedMessage[0], treatedMessage[2])
     }
-    else if (treatedMessage[0] === 'guarda'){
+    else if (treatedMessage[0] === 'guarda' && treatedMessage.length == 2){
       this.commandManager.rollServantGuard(message, treatedMessage[1])
     }
-    else if (treatedMessage[0] === 'armar'){
+    else if (treatedMessage[0] === 'armar' && treatedMessage.length == 2){
       this.commandManager.armServant(message, treatedMessage[1])
     }
     else{
